d1dc2a41781898c77acd5f8e9bce063ef27952a2
Priscilla Okawa <prisoka@gmail.com>
2018-04-09 11:10:51 -0700

    solved basic-js problems

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 module.exports = {
-  difference: function(){
-
+  difference: function(a,b){
+    return a-b;
   },
-  isEqual: function(){
-
+ 
+  isEqual: function(a,b){
+    return a===b;
   },
-  isEven: function(){
-
+ 
+  isEven: function(num){
+    return num%2 === 0;
   },
-  isDivisible: function(){
-
+ 
+  isDivisible: function(a,b){
+    return a%b === 0;
   },
-  discountPercentage: function(){
-
+ 
+  discountPercentage: function(amount, discount){
+    if(discount>100 || discount<0){
+      return "please enter a number between 0 and 100"
+    } else {
+      return (amount*discount/100);
+    }
   },
-  isVowel: function(){
-
+ 
+  isVowel: function(letter){
+    if (letter === 'a'|| letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u'){
+      return true;
+    }else{
+      return false;
+    }
   },
-  celsiusToFahrenheit: function(){
-
+ 
+  celsiusToFahrenheit: function(celsius){
+    return celsius*1.8+32;
   },
-  biggestOfThree: function(){
-
+ 
+  biggestOfThree: function(a, b, c){
+    return Math.max(a, b, c);
   },
+ 
   // Bonus
-  federalIncomeTaxCalculator: function(){
-
+  federalIncomeTaxCalculator: function(salary){
+    salary = new Intl.NumberFormat().format(salary);
+    return salary*0.396;
   }
 };
